e5dee1421d8b91479fab913dbef07c6f2b62ef5b
MS\siovino <silvanalorena@gmail.com>
2025-10-27 15:50:44 -0300

    Added test data

---
+++ test/data/constants.js
@@ -0,0 +1,19 @@
+const imageLocation = {
+  valid_image_320x320: 'test/data/images/image_320x320.jpg',
+  image_to_edit_320x320: 'test/data/images/image_to_edit.jpg'
+}
+
+const imageName = {
+  valid_image_320x320: 'image_320x320.jpg',
+  image_to_edit_320x320: 'image_to_edit.jpg'
+}
+
+const deleteModal = {
+  deleteConfirmationText: 'Are you sure you want to delete this item?'
+}
+
+module.exports = {
+    imageLocation,
+    imageName,
+    deleteModal
+}
